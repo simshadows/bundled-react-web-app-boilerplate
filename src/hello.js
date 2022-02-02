31075fc8cf0a9a8b13ebfe3d93c96a5ff6d20228
simshadows <contact@simshadows.com>
2022-02-02 20:57:34 +1100

    Installed 'webpack' and 'webpack-cli'
    I pretty much ran:
        yarn add webpack --dev
        yarn add webpack-cli --dev
    and made some minor changes to get some basic things working.

--- src/hello.js
+++ src/hello.js
@@ -1,4 +1,4 @@
-export function getMessage() {
-    return "I have no idea what I'm doing!";
+export function getMessage(num) {
+    return `I have no idea what I'm doing! (Version ${num + 1})`;
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class TestComponent extends React.Component {
             {
             id: "hello",
             },
-            element("span", null, getMessage()),
+            element("span", null, getMessage(0)),
         );
     }
 }
